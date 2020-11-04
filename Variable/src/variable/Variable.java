cfe037c756772b0a88976dfca1435f940a4a4c58
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 02:42:15 +0700

    Add files via upload

---
+++ Variable/src/variable/Variable.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variable;
+
+/**
+ * @author  @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-2
+ * Nim   : 10116378
+ */
+public class Variable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
